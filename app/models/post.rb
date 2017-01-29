8076d812986241357d12b634d732c7863eef687e
Sea-Anna <seaanna.thompson@gmail.com>
2017-01-29 08:25:34 -0800

    added in comment to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  # post gets deleted, so does the comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
 end
